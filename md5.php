0d1dae9ef1a9ab68a38dfa582604bde8b87d4ef2
Sven Schrodt <sven@schrodt-service.net>
2021-02-08 18:53:01 +0100

    Deleting unneeded resources

--- md5.php
+++
@@ -1,8 +0,0 @@
-<pre>
-<?php
-
-echo md5( "What was the first Call of Duty game to include the Zombies gamemode?");
-echo PHP_EOL;
-
-echo md5('What was the first Call of Duty game to include the Zombies gamemode?');
-echo PHP_EOL;
